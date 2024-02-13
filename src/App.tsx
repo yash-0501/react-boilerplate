ee52d11f9e0f40e2299dee7e4970161b7569e36b
Yash Agarwal <say2yasha2000@gmail.com>
2024-02-13 12:51:37 +0530

    test-2

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(10)
 
   return (
     <>
@@ -18,8 +18,8 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 2)}>
-          Updated count is {count}
+        <button onClick={() => setCount((count) => count + 1)}>
+          New count is {count}
         </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
